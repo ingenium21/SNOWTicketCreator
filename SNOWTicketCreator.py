8065929083fd26bddb7d15936cb6954ad400d740
Renato Regalado <renato.regalado@gmail.com>
2021-04-29 15:15:39 -0500

    Create SNOWTicketCreator.py

---
+++ SNOWTicketCreator.py
@@ -0,0 +1,6 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Chrome("""C:\Program Files (x86)\Google\Chrome\Application\chrome.exe""")
